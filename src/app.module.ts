c4451442f18895b53ee3d4653e6e53a51abe1023
Ali Bayramli <bayramliali@tutanota.com>
2023-08-13 15:31:39 +0400

    App module base setup

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,23 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
+import { APP_GUARD, APP_INTERCEPTOR } from '@nestjs/core';
+import { PrismaModule } from './prisma/prisma.module';
+import { HomeModule } from './home/home.module';
+import { UserModule } from './user/user.module';
+import { UserInterceptor } from './user/interceptors/user.interceptor';
 import { AppService } from './app.service';
+import { AppController } from './app.controller';
+import { AuthGuard } from './guards/auth.guard';
 
 @Module({
-  imports: [],
+  imports: [UserModule, PrismaModule, HomeModule],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [
+    AppService,
+    {
+      provide: APP_INTERCEPTOR,
+      useClass: UserInterceptor,
+    },
+    { provide: APP_GUARD, useClass: AuthGuard },
+  ],
 })
 export class AppModule {}
